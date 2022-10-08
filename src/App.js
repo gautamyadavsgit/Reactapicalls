315630b59e11663231b305f127f8b9644c99e653
gautamyadavsgit <gautaminit@gmail.com>
2022-10-08 18:07:12 +0530

    used async await

--- src/App.js
+++ src/App.js
@@ -5,20 +5,20 @@ import './App.css';
 
 function App() {
   const [movies, setMovies] = useState([]);
-  function fetchMoviesHandler(){
-    fetch('https://swapi.dev/api/films').then(response => {
-             return response.json();
-    }).then(data => {
+  async function fetchMoviesHandler(){
+     const response = await fetch('https://swapi.dev/api/films')
+     const data =  await response.json();
       const transformMovies = data.results.map(movieData => {
            return {
             id:movieData.episode_id,
             title:movieData.title,
             openingText:movieData.opening_crawl,
             releaseDate:movieData.release_date
-           }
-      });
-      setMovies(transformMovies);
+           };
+     
+     
     });
+    setMovies(transformMovies);
   }
   return (
     <React.Fragment>
